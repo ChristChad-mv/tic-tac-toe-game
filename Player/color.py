5adb0daadc529d0f02dd2cb4d9213f5d9f7e4022
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2024-12-30 21:21:43 +0100

    feature : Choosing color

---
+++ Player/color.py
@@ -0,0 +1,11 @@
+
+from enum import Enum
+class TColor(Enum):
+    """
+    Énumération des couleurs disponibles pour les joueurs.
+    """
+    ROUGE = "rouge"
+    BLEU = "bleu"
+    VERT = "vert"
+    JAUNE = "jaune"
+    VIDE = " "  # Utilisé pour les cases vides
