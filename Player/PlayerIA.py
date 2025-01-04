fd5fb13f8482789c83b327114ae88641d9ed3a71
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-01-04 03:39:13 +0100

    Refactor TPlayerIA class: Updated constructor parameter name for clarity, added method to retrieve possible moves based on the game state

--- Player/PlayerIA.py
+++ Player/PlayerIA.py
@@ -1,10 +1,12 @@
 from Player.player import TPlayer
 from typing import List, Tuple
 from Player.color import TColor
+from game_logic import GameLogic  
+from copy import deepcopy
 
 class TPlayerIA(TPlayer):
     """
-    Classe représentant un joueur IA (Intelligence Artificielle).
+    Classe représentant un joueur IA
 
     Hérite de :
     - TPlayer : Reprend les attributs communs à tous les joueurs.
@@ -14,37 +16,24 @@ class TPlayerIA(TPlayer):
     - Implémente Minimax pour choisir les coups automatiquement.
     """
 
-    def __init__(self, cName: str):
+    def __init__(self, cPLRName: str):
         """
         Initialise un joueur IA.
 
-        @param cName : Nom de l'IA (par ex. "Ordinateur")
+        @param cPLRName : Nom de l'IA (par ex. "Ordinateur")
         """
-        super().__init__(cName, bIsAI=True)
+        super().__init__(cPLRName, bIsAI=True)
 
-    def PLRjouer(self):
+    def get_possible_moves(self, game: GameLogic) -> List[Tuple[int, int]]:
         """
-        Joue automatiquement le meilleur coup.
+        Retourne toutes les cases vides de la grille comme coups possibles.
 
-        L'IA utilise Minimax pour choisir la meilleure option disponible.
+        @param game : Instance actuelle du jeu
+        @return : Liste des coordonnées des cases disponibles
         """
-        pass
-
-    def PLRfind_best_move(self, tGrid: List[List[TColor]]) -> Tuple[int, int]:
-        """
-        Trouve la meilleure position où jouer en utilisant Minimax.
-
-        @param tGrid : Grille actuelle du jeu
-        @return : Tuple contenant (ligne, colonne) du meilleur coup à jouer
-        """
-        pass
-
-    def PLRminimax(self, tGrid: List[List[TColor]], bIsMaximizing: bool) -> int:
-        """
-        Implémente l'algorithme Minimax pour évaluer les coups possibles.
-
-        @param tGrid : Grille du jeu
-        @param bIsMaximizing : True si l'IA maximise, False si elle minimise
-        @return : Valeur numérique représentant l'évaluation du coup
-        """
-        pass
+        moves = []
+        for row in range(game.iGLIsize):
+            for col in range(game.iGLIsize):
+                if game.tGLIgrid[row][col] == TColor.VIDE:
+                    moves.append((row, col))
+        return moves
