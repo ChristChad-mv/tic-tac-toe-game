391918c14ffac88db410e70d07c2b5f3771d4e37
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-01-10 07:12:23 +0100

    Refactor TColor Enum: Updated color representation from strings to integers for improved performance and added tkinter_color property for Tkinter compatibility.

--- Player/color.py
+++ Player/color.py
@@ -1,11 +1,22 @@
-
 from enum import Enum
+
 class TColor(Enum):
-    """
-    Énumération des couleurs disponibles pour les joueurs.
-    """
-    ROUGE = "rouge"
-    BLEU = "bleu"
-    VERT = "vert"
-    JAUNE = "jaune"
-    VIDE = " "  # Utilisé pour les cases vides
+    VIDE = 0
+    ROUGE = 1
+    BLEU = 2
+    VERT = 3
+    JAUNE = 4
+    ORANGE = 5
+
+    @property
+    def tkinter_color(self):
+        """Retourne la couleur Tkinter correspondante"""
+        color_mapping = {
+            TColor.VIDE: "white",
+            TColor.ROUGE: "red",
+            TColor.BLEU: "blue",
+            TColor.VERT: "green",
+            TColor.JAUNE: "yellow",
+            TColor.ORANGE: "orange"
+        }
+        return color_mapping[self]
